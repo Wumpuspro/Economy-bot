7b5a2f93b160f5b697f10785945b45f3e237cfd8
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-09 21:59:51 +0545

    credit to androz

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+{
+    "token":"your discord bot secret token",
+    "prefix":"!",
+    "game":"made by Wumpus",
+    "embed":{
+        "color":"#ff5b5b",
+        "footer":"NPGOP"
+    },
+    "administrators":[
+        "AdminID",
+        "AdminID"
+    ]
+}
